507fbb6870e345e98e687f4e40203ba59a3558c5
Wendy Hamiltonquinn  Pronoun (She/Her) <wendyhamiltonquinn@outlook.com>
2022-07-04 00:18:57 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -91,11 +91,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+let array = totalArray(multArr [0], multArr[1], multArr[2];
+  return [array[1], 'The # ${multArr} have a product of ${array[1]}. ];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
