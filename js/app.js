961a3d1329470edb5eca58dd88b350e7c1ffdf11
Wendy Hamiltonquinn  Pronoun (She/Her) <wendyhamiltonquinn@outlook.com>
2022-07-04 00:04:03 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -48,11 +48,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+let sum = sum(sum(a,b) [0] , c) [0];
+  let 1multi = multiply (a,b);
+  let 2multi = multipy (c, 1multi [0]) [0];
 }
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+// Here is the test for sumAndMultiply(); unomment it to run it
+testSumAndMultiply(4,7,5);
+ (a,b);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
